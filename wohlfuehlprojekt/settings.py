a19b5d98f7dc715f7353542b12e14dfaf5de4e25
TwentyOneLunex <krassover@web.de>
2017-02-01 10:13:46 +0100

    Routing Bugs korrigiert

--- wohlfuehlprojekt/settings.py
+++ wohlfuehlprojekt/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'wohlfuehlprojekt.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testdb',
-        'USER': 'testadmin',
-        'PASSWORD': 'qwertz',
+        'NAME': 'localdb',
+        'USER': 'lunex',
+        'PASSWORD': 'lpw',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
